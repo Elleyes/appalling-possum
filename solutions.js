d5317312ee113a97bd81b29cea4c91d0864bdb6a
Elleyes <Elleyes@users.noreply.github.com>
2016-12-09 16:53:08 -0800

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,6 +1,7 @@
-Solutions for Freecodecamp Basic Algorithms
+SOLUTIONS FOR FREECODECAMP Basic Algorithms
 
-Reverse a String:
+
+Reverse a String | NO ES6 | :
 
 function reverseString(str) {
   return str.split("").reverse().join("");
@@ -21,10 +22,36 @@ function factorialize(num) {
 factorialize(5);
 
 
+FACTORIRALIZE A NUMBER | ES6 | :
+
+function factorialize(num) {
+  let fact=1;
+  for (let i = 2; i <= num; i++)
+      fact = fact * i;
+  return fact;
+}
+
+factorialize(5);
+
+
 Check for Palindromes:
 
 function palindrome(str) {
   var pal = str.replace(/[\W_]/g, "").toLowerCase();// Good luck!
+  if (pal === pal.split('').reverse().join('')) {
+  return true;
+  } else {
+    return false;
+  }
+}
+
+palindrome("eye");
+
+
+CHECK FOR PALINDROMES | ES6 | :
+	
+function palindrome(str) {
+  const pal = str.replace(/[\W_]/g, "").toLowerCase();// Good luck!
   if (pal === pal.split('').reverse().join('')) {
   return true;
   } else {
@@ -51,6 +78,22 @@ function findLongestWord(str) {
 findLongestWord("The quick brown fox jumped over the lazy dog");
 
 
+FIND THE LONGEST WORD IN A STRING | ES6 | :
+
+function findLongestWord(str) {
+  strSplit = str.split(' ');
+  let longestWord = 0;
+  for (let i = 0; i < strSplit.length; i++) {
+    if(strSplit[i].length > longestWord) {
+	longestWord = strSplit[i].length;
+    }
+  }
+  return longestWord;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
 Title Case a Sentence:
 
 function titleCase(str) {
@@ -65,7 +108,21 @@ function titleCase(str) {
 titleCase("I'm a little tea pot");
 
 
-Return Largest Numbers in Arrays:
+TITLE CASE A SENTENCE | ES6 | :
+
+function titleCase(str) {
+  const lower = str.toLowerCase();
+
+  function title(match) {
+    return match.replace(match.charAt(0), match.charAt(0).toUpperCase());
+  }
+  return lower.replace(/\w\S*/g, title);
+}
+
+titleCase("I'm a little tea pot");
+
+
+Return Largest Numbers in Arrays | NO ES6 |:
 
 function largestOfFour(arr) {
   // You can do this!
@@ -75,7 +132,7 @@ function largestOfFour(arr) {
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 
-Confirm the Ending:
+Confirm the Ending | NO ES6 | :
 
 function confirmEnding(str, target) {
 
@@ -104,7 +161,20 @@ function repeatStringNumTimes(str, num) {
 
 repeatStringNumTimes("abc", 3);
 
-Truncate a String:
+
+REPEAT A STRING REPEAT A STRING | ES6 | :
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  const repeated = [];
+  for (i = 0; i < num; i++) {
+    repeated.push(str);
+    }
+  return repeated.join('');
+}
+
+
+Truncate a String | NO ES6 | :
 
 function truncateString(str, num) {
   // Clear out that junk in your trunk
@@ -137,7 +207,23 @@ function chunkArrayInGroups(arr, size) {
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 
-Slasher Flick:
+CHUNKY MONKEY | ES6 | :
+
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  const newArr = [];
+  let i = 0;
+  while (i < arr.length) {
+    newArr.push(arr.slice(i, i + size));
+    i += size;
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+Slasher Flick | NO ES6 | :
 
 function slasher(arr, howMany) {
   // it doesn't always pay to be first
@@ -161,7 +247,18 @@ function mutation(arr) {
 mutation(["hello", "hey"]);
 
 
-Falsy Bouncer:
+MUTATIONS | ES6 | :
+
+function mutation(arr) {
+  return arr[1].toLowerCase()
+    .split('')
+    .every(letter => arr[0].toLowerCase().includes(letter));
+}
+
+mutation(["hello", "hey"]);
+
+
+Falsy Bouncer | NO ES6 | :
 
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
@@ -182,8 +279,19 @@ function destroyer(arr) {
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
+SEEK AND DESTROY | ES6 | :
 
-Where do I Belong:
+function destroyer(arr) {
+  // Remove all the values
+  args = Array.prototype.slice.call(arguments);
+  args.splice(0, 1);
+  return arr.filter(element => !args.includes(element));
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+
+Where do I Belong | NO ES6 | :
 
 function getIndexToIns(arr, num) {
   // Find my place in this sorted array.
